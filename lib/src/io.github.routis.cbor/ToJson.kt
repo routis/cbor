db94352d9eebd74c90d514d77c52fa5d9460aa66
routis <routis@protonmail.com>
2023-11-22 22:11:11 +0200

    Added options to toJson()

--- lib/src/io.github.routis.cbor/ToJson.kt
+++ lib/src/io.github.routis.cbor/ToJson.kt
@@ -5,44 +5,75 @@ import java.math.BigInteger
 import kotlin.io.encoding.Base64
 
 
-fun toJson(cbor: DataItem): JsonElement {
+typealias KeyMapper<K> = (K) -> String?
+
+data class KeyOptions(
+        val booKeyMapper: KeyMapper<Key.BoolKey>? = null,
+        val byteStringKeyMapper: KeyMapper<Key.ByteStringKey>? = null,
+        val negativeIntegerKeyMapper: KeyMapper<Key.NegativeIntegerKey>? = null,
+        val unsignedIntegerKeyMapper: KeyMapper<Key.UnsignedIntegerKey>? = null
+) {
+    companion object {
+        val UseOnlyTextKey: KeyOptions = KeyOptions(
+                booKeyMapper = null,
+                byteStringKeyMapper = null,
+                negativeIntegerKeyMapper = null,
+                unsignedIntegerKeyMapper = null
+        )
+    }
+}
+
+data class ToJsonOptions(
+        val keyOptions: KeyOptions
+) {
+    companion object {
+        val Default = ToJsonOptions(keyOptions = KeyOptions.UseOnlyTextKey)
+    }
+}
+
+fun toJson(cbor: DataItem, options: ToJsonOptions = ToJsonOptions.Default): JsonElement {
     return when (cbor) {
         is DataItem.UnsignedInteger -> JsonPrimitive(cbor.value)
         is DataItem.NegativeInteger -> JsonPrimitive(cbor.value)
         is DataItem.ByteString -> JsonPrimitive(Base64.UrlSafe.encode(cbor.bytes))
         is DataItem.TextString -> JsonPrimitive(cbor.text)
-        is DataItem.Array -> cbor.mapNotNull { toJson(it) }.let(::JsonArray)
+        is DataItem.Array -> cbor.mapNotNull { toJson(it, options) }.let(::JsonArray)
         is DataItem.CborMap -> {
+            val keyOptions = options.keyOptions
             return cbor.mapNotNull { (k, v) ->
+
                 val jsonKey = when (k) {
-                    is Key.BoolKey -> k.item.value.toString()
-                    is Key.ByteStringKey -> Base64.UrlSafe.encode(k.item.bytes)
-                    is Key.NegativeIntegerKey -> k.item.value.toString()
-                    is Key.UnsignedIntegerKey -> k.item.value.toString()
+                    is Key.BoolKey -> keyOptions.booKeyMapper?.invoke(k)
+                    is Key.ByteStringKey -> keyOptions.byteStringKeyMapper?.invoke(k)
+                    is Key.NegativeIntegerKey -> keyOptions.negativeIntegerKeyMapper?.invoke(k)
+                    is Key.UnsignedIntegerKey -> keyOptions.unsignedIntegerKeyMapper?.invoke(k)
                     is Key.TextStringKey -> k.item.text
                 }
-                val jsonValue = toJson(v) ?: JsonNull
-                jsonKey to jsonValue
+                
+                if (jsonKey != null) jsonKey to toJson(v, options)
+                else null
+
             }.toMap().let(::JsonObject)
         }
 
         is DataItem.Tagged<*> -> when (cbor) {
-            is DataItem.Tagged.StandardDateTimeString -> toJson(cbor.content)
-            is DataItem.Tagged.EpochBasedDateTime.Unsigned -> toJson(cbor.content)
-            is DataItem.Tagged.EpochBasedDateTime.Negative -> toJson(cbor.content)
-            is DataItem.Tagged.EpochBasedDateTime.HalfFloat -> toJson(cbor.content)
-            is DataItem.Tagged.EpochBasedDateTime.SingleFloat -> toJson(cbor.content)
-            is DataItem.Tagged.EpochBasedDateTime.DoubleFloat -> toJson(cbor.content)
+            is DataItem.Tagged.StandardDateTimeString -> toJson(cbor.content, options)
+            is DataItem.Tagged.EpochBasedDateTime.Unsigned -> toJson(cbor.content, options)
+            is DataItem.Tagged.EpochBasedDateTime.Negative -> toJson(cbor.content, options)
+            is DataItem.Tagged.EpochBasedDateTime.HalfFloat -> toJson(cbor.content, options)
+            is DataItem.Tagged.EpochBasedDateTime.SingleFloat -> toJson(cbor.content, options)
+            is DataItem.Tagged.EpochBasedDateTime.DoubleFloat -> toJson(cbor.content, options)
             is DataItem.Tagged.BigNumUnsigned -> JsonPrimitive(cbor.asNumber())
             is DataItem.Tagged.BigNumNegative -> JsonPrimitive(cbor.asNumber())
             is DataItem.Tagged.DecimalFraction -> TODO("Implement toJson for DecimalFraction")
             is DataItem.Tagged.BigFloat -> TODO("Implement toJson for BigFloat")
-            is DataItem.Tagged.EncodedText -> toJson(cbor.content)
-            is DataItem.Tagged.DborDataItem -> toJson(decode(cbor.content.bytes))
-            is DataItem.Tagged.SelfDescribedCbor -> toJson(cbor.content)
+            is DataItem.Tagged.EncodedText -> toJson(cbor.content, options)
+            is DataItem.Tagged.DborDataItem -> toJson(decode(cbor.content.bytes), options)
+            is DataItem.Tagged.SelfDescribedCbor -> toJson(cbor.content, options)
             is DataItem.Tagged.Unassigned -> JsonNull
-            is DataItem.Tagged.FullDateTime -> toJson(cbor.content)
+            is DataItem.Tagged.FullDateTime -> toJson(cbor.content, options)
         }
+
         is DataItem.Bool -> JsonPrimitive(cbor.value)
         is DataItem.HalfPrecisionFloat -> JsonPrimitive(cbor.value)
         is DataItem.SinglePrecisionFloat -> JsonPrimitive(cbor.value)
@@ -55,5 +86,5 @@ fun toJson(cbor: DataItem): JsonElement {
 }
 
 private fun DataItem.Tagged.BigNumUnsigned.asNumber(): Number = content.asBigInteger() // TODO this is java specific
-private fun DataItem.Tagged.BigNumNegative.asNumber(): Number = - BigInteger.ONE - content.asBigInteger() // TODO this is java specific
-private fun DataItem.ByteString.asBigInteger() : BigInteger = BigInteger(bytes) // TODO this is java specific
+private fun DataItem.Tagged.BigNumNegative.asNumber(): Number = -BigInteger.ONE - content.asBigInteger() // TODO this is java specific
+private fun DataItem.ByteString.asBigInteger(): BigInteger = BigInteger(bytes) // TODO this is java specific
