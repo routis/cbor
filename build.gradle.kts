00ead883fa7d6cbdb3730acfa1aefcf9576d4aea
routis <>
2023-12-06 16:34:44 +0200

    Renabled the ktlint checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,11 @@ plugins {
     kotlin("multiplatform").version(libs.versions.kotlin).apply(false)
     kotlin("plugin.serialization").version(libs.versions.kotlin).apply(false)
     alias(libs.plugins.ktlint).apply(false)
-//    alias(libs.plugins.detekt).apply(false)
 }
 
 allprojects {
     group = "io.github.routis"
     version = "0.0.1-SNAPSHOT"
-
-    repositories {
-        mavenCentral()
-        google()
-        mavenLocal()
-    }
 }
 
 subprojects {
@@ -23,8 +16,7 @@ subprojects {
     configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
         debug.set(true)
         verbose.set(true)
-        ignoreFailures.set(true)
+        outputToConsole.set(true)
+
     }
-
-
 }
