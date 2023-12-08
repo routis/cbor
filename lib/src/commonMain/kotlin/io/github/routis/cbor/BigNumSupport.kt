95547780f5f1dc6ba93ed7a6819a67b840ced621
routis <>
2023-12-08 14:03:09 +0200

    Factored out a common method

--- lib/src/commonMain/kotlin/io/github/routis/cbor/BigNumSupport.kt
+++ lib/src/commonMain/kotlin/io/github/routis/cbor/BigNumSupport.kt
@@ -3,15 +3,18 @@ package io.github.routis.cbor
 import com.ionspin.kotlin.bignum.integer.BigInteger
 import com.ionspin.kotlin.bignum.integer.Sign
 
+
 fun DataItem.Integer.asBigInteger(): BigInteger =
     when (this) {
         is DataItem.Integer.Unsigned -> BigInteger.fromULong(value)
         is DataItem.Integer.Negative -> BigInteger.fromULong(value).toCborNegative()
     }
 
-fun DataItem.Tagged.BigNumUnsigned.asBigInteger(): BigInteger = BigInteger.fromByteArray(content.bytes, Sign.POSITIVE)
+fun DataItem.Tagged.BigNumUnsigned.asBigInteger(): BigInteger = BigInteger.fromByteString(content)
 
-fun DataItem.Tagged.BigNumNegative.asBigInteger(): BigInteger = BigInteger.fromByteArray(content.bytes, Sign.POSITIVE).toCborNegative()
+fun DataItem.Tagged.BigNumNegative.asBigInteger(): BigInteger = BigInteger.fromByteString(content).toCborNegative()
+
+private fun BigInteger.Companion.fromByteString(byteString: DataItem.ByteString) = fromByteArray(byteString.bytes, Sign.POSITIVE)
 
 private fun BigInteger.toCborNegative(): BigInteger {
     check(this >= BigInteger.ZERO) { "Applicable to integers greater equal to zero" }
